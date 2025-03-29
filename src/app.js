3f975b6dec73ae13af2d1f81d510df4bea845778
Varshipk <varshipkatiyar@gmail.com>
2025-03-29 21:21:31 +0530

    backend service

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.js";
+import profileRouter from "./routes/profile.js";
+import requestRouter from "./routes/request.js";
+import userRouter from "./routes/user.js";
+import cors from "cors";
+const app = express();
+import "dotenv/config";
+const PORT = 3000;
+import { dbConnect } from "./config/database.js";
+dbConnect();
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with frontend URL
+    credentials: true, // Allow cookies
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
+app.listen(PORT, () => {
+  console.log("Server is listening");
+});
